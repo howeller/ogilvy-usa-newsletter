6354971c72ed655c73996fd3211e8aff198f85b5
Sanjay Cherubala <sanjay.cherubala@ogilvy.com>
2019-10-23 13:18:31 -0400

    Interim controller updates

--- index.php
+++ index.php
@@ -11,9 +11,37 @@ try {
   exit;
 }
 
+$data = new stdClass();
+$data->global = new stdClass();
+
 showHeader( 'text' );
 
 print_r( $sheet->getRowData() );
+foreach( $sheet->getRowData() as $row ) {
+  $name = strtoupper( str_replace( ' ', '_', $row['NAME'] ) );
+  switch( $row['SECTION'] ) {
+    case 'GLOBAL':
+      $data->global->$name = $row['TITLE'];
+      break;
+    
+    case 'BODY':
+      $module = new stdClass();
+      $module->name = $name;
+      $module->title = $row['TITLE'];
+      $module->subtitle = $row['SUBTITLE'];
+      $module->bodycopy = $row['BODY COPY'];
+      $module->image = $row['IMAGE'];
+      $module->linkurl = $row['LINK URL'];
+      $data->modules[] = $module;
+      break;
+    
+    default:
+      // ERROR
+      break;
+  }
+}
+
+print_r( $data );
 
 
 ?>
