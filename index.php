2543062371ade0fb396cbb150da48237479c63ad
Sanjay Cherubala <sanjay.cherubala@ogilvy.com>
2019-10-23 15:59:18 -0400

    Updated controller

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 require_once( 'lib/dxa_smartsheet.inc' );
 require_once( 'lib/dxa_utility.inc' );
 require_once( 'lib/config.inc' );
+require( 'vendor/autoload.php' );
+
 $config = $GLOBALS['config'];
+
+function makeModule( $row ) {
+  $name = strtoupper( str_replace( ' ', '_', $row['NAME'] ) );
+  
+  $module = new stdClass();
+  $module->NAME = $name;
+  $module->TITLE = $row['TITLE'];
+  $module->SUBTITLE = $row['SUBTITLE'];
+  $module->BODYCOPY = $row['BODY COPY'];
+  $module->IMAGE = $row['IMAGE'];
+  $module->LINKURL = $row['LINK URL'];
+  
+  return( $module );
+}
 
 try {
   $sheet = new SS_Sheet( $config->key, $config->contentSheetId );
@@ -13,26 +29,27 @@ try {
 
 $data = new stdClass();
 $data->global = new stdClass();
+$data->body = [];
+$data->bottom = [];
 
-showHeader( 'text' );
 
-//print_r( $sheet->getRowData() );
 foreach( $sheet->getRowData() as $row ) {
   $name = strtoupper( str_replace( ' ', '_', $row['NAME'] ) );
   switch( $row['SECTION'] ) {
     case 'GLOBAL':
-      $data->global->$name = $row['TITLE'];
+      if( $name == 'HERO' ) {
+        $data->global->$name = makeModule( $row );
+      } else {
+        $data->global->$name = $row['TITLE'];
+      }
+      break;
+
+    case 'BODY':
+      $data->body[] = makeModule( $row );
       break;
     
-    case 'BODY':
-      $module = new stdClass();
-      $module->NAME = $name;
-      $module->TITLE = $row['TITLE'];
-      $module->SUBTITLE = $row['SUBTITLE'];
-      $module->BODYCOPY = $row['BODY COPY'];
-      $module->IMAGE = $row['IMAGE'];
-      $module->LINKURL = $row['LINK URL'];
-      $data->modules[] = $module;
+    case 'BOTTOM':
+      $data->bottom[] = makeModule( $row );
       break;
     
     default:
@@ -41,7 +58,10 @@ foreach( $sheet->getRowData() as $row ) {
   }
 }
 
+showHeader( 'text' );
 print_r( $data );
+exit;
+print renderTemplate( 'templates/email_template_sc.tpl', $data );
 
 
 ?>
